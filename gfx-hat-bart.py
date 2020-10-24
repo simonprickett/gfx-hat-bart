86f24c5e38e713c99f5e251187c5c97d4084e8b0
Simon Prickett <simon@crudworks.org>
2020-10-24 13:17:23 -0700

    Fixed upward movement on menu.

--- gfx-hat-bart.py
+++ gfx-hat-bart.py
@@ -9,6 +9,8 @@ from PIL import Image, ImageFont, ImageDraw
 API_BASE = 'http://api.bart.gov/api'
 API_KEY = os.getenv('BART_API_KEY', 'MW9S-E7SL-26DU-VV8V')
 
+BAR_LOCATION = 2
+
 width, height = lcd.dimensions()
 font = ImageFont.truetype(fonts.BitbuntuFull, 10)
 image = Image.new('P', (width, height))
@@ -16,7 +18,7 @@ draw = ImageDraw.Draw(image)
 
 stations = {}
 menu_options = []
-current_menu_option = 2
+current_menu_option = BAR_LOCATION
 
 class MenuOption:
     def __init__(self, name, action, options=()):
@@ -74,8 +76,6 @@ def button_press_handler(ch, event):
         if current_menu_option == len(menu_options):
             current_menu_option = len(menu_options) - 1
 
-    print(f'current_menu_option = {current_menu_option}')
-
 def setup_touch_buttons():
     for x in range(6):
         touch.set_led(x, 0)
@@ -90,19 +90,37 @@ def show_station_picker():
     for stationAbbr in stations:
         menu_options.append(MenuOption(stations[stationAbbr], show_departures, (stationAbbr))) 
 
-    image.paste(0, (0, 0, width, height))
-
     while True:
-        for index in range(len(menu_options)):
+        line = 0
+        image.paste(0, (0, 0, width, height))
+
+        start_pos = current_menu_option - BAR_LOCATION 
+
+        print(f'start_pos -> {start_pos}')
+
+        if start_pos < 0:
+            line = abs(start_pos)
+            start_pos = 0
+
+        end_pos = current_menu_option + 4
+ 
+        if end_pos >= len(menu_options):
+            end_pos = len(menu_options) -1
+
+        for index in range(start_pos, end_pos):
+            print(index)
             x = 7 
-            y = (index * 12)
+            y = (line * 12)
             option = menu_options[index]
 
-            if index == current_menu_option:
+            if line  == BAR_LOCATION:
                 draw.rectangle(((x - 2, y - 1), (width, y + 10)), 1)
 
-            draw.text((x, y), option.name, 0 if index == current_menu_option else 1, font) 
+            print(option.name)
+            draw.text((x, y), option.name, 0 if line == BAR_LOCATION else 1, font) 
+            line += 1
 
+        print('----')
         w, h = font.getsize('>')
         draw.text((0, ((height - h) / 2) - 3), '>', 1, font)
 
